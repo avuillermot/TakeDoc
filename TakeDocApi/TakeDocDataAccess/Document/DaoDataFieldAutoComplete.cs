e1d7bd6bb95bcb3814c3e163d499c3131a7c4fa6
avuillermot <avuillermot@hotmail.com>
2015-03-23 12:20:52 +0100

    datafieldautocomplete object

---
+++ TakeDocApi/TakeDocDataAccess/Document/DaoDataFieldAutoComplete.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeDocDataAccess.Document
+{
+    public class DaoDataFieldAutoComplete : DaoBase<TakeDocModel.DataFieldAutoComplete>, Interface.IDaoDataFieldAutoComplete
+    {
+        public ICollection<TakeDocModel.DataFieldAutoComplete> GetBy(Expression<Func<TakeDocModel.DataFieldAutoComplete, bool>> where, params Expression<Func<TakeDocModel.DataFieldAutoComplete, object>>[] properties)
+        {
+            return base.GetBy(where, properties);
+        }
+    }
+}
---
+++ TakeDocApi/TakeDocDataAccess/Document/Interface/IDaoDataFieldAutoComplete.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeDocDataAccess.Document.Interface
+{
+    public interface IDaoDataFieldAutoComplete
+    {
+        ICollection<TakeDocModel.DataFieldAutoComplete> GetBy(Expression<Func<TakeDocModel.DataFieldAutoComplete, bool>> where, params Expression<Func<TakeDocModel.DataFieldAutoComplete, object>>[] properties);
+    }
+}
