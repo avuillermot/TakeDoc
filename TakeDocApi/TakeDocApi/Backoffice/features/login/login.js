5964ca95d68df3578cdfedcab19253ef8c1bb129
avuillermot <avuillermot@hotmail.com>
2015-06-11 23:59:49 +0200

    new style

--- TakeDocApi/TakeDocApi/Backoffice/features/login/login.js
+++ TakeDocApi/TakeDocApi/Backoffice/features/login/login.js
@@ -1,18 +1,16 @@
 ﻿'use strict';
 backOffice.controller('loginController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
 
-    $scope.identity = {
+    /*$scope.identity = {
         login: "avuillermot@gmail.com",
         password: "password"
-    };
+    };*/
 
     $scope.onKeyPress = function (event) {
         if (event.keyCode == 13) this.doCheck();
     };
 
     $scope.doCheck = function () {
-
-        $rootScope.showLoader('Connexion...');
 
         var error = function () {
             var data = arguments[0].responseJSON;
@@ -25,7 +23,11 @@ backOffice.controller('loginController', ['$scope', '$rootScope', '$location', f
             $location.path("/home");
             $scope.$apply();
         };
-        userTkService.logon($scope.identity.login, $scope.identity.password, success, error);
+
+        if ($scope.identity != null && $scope.identity.login != "" && $scope.identity.password != "") {
+            $rootScope.showLoader('Connexion...');
+            userTkService.logon($scope.identity.login, $scope.identity.password, success, error);
+        }
         return false;
     }
 
