37f0a65a9c942155e80eb27d6e2227d4482838d8
avuillermot <avuillermot@hotmail.com>
2015-07-02 22:01:57 +0200

    search metadata

---
+++ TakeDocApi/TakeDocModel/Dto/Document/SearchMetadata.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeDocModel.Dto.Document
+{
+    public class SearchMetadata : TakeDocModel.MetaData
+    {
+        public string Condition;
+        public SearchMetadata(string condition)
+        {
+            Condition = condition;
+        }
+    }
+
+    public static class SearchCondition
+    {
+        public static string Equals
+        {
+            get
+            {
+                return "=";
+            }
+        }
+
+        public static string Greater
+        {
+            get
+            {
+                return ">";
+            }
+        }
+
+        public static string Less
+        {
+            get
+            {
+                return "<";
+            }
+        }
+
+        public static string Start
+        {
+            get
+            {
+                return "START";
+            }
+        }
+
+        public static string Contains
+        {
+            get
+            {
+                return "CONTAINS";
+            }
+        }
+    }
+}
