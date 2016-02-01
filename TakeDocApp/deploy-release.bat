ea2ec2f51e6fcdedbe6d493de7d5e6fa26d01230
avuillermot <avuillermot@hotmail.com>
2016-02-01 21:47:14 +0100

    saisie ergo

--- TakeDocApp/deploy-release.bat
+++ TakeDocApp/deploy-release.bat
@@ -1,2 +1,2 @@
 adb uninstall com.ionicframework.takedocapp756299
-adb install platforms\android\build\outputs\apk\android-release-unsigned.apk
+adb install platforms\android\build\outputs\apk\android-release.apk
